ef366e135022076f29052bf720f6365edc79598e
Bouameurwadie <Bouameur.wadie@gmail.com>
2022-08-29 20:44:44 +0100

    Ajoutez des fichiers projet.

---
+++ FrmDC.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.Common;
+
+namespace AdoNet_V3
+{
+    public partial class FrmDC : Form
+    {
+        SqlConnection ctn = new SqlConnection(@"Data Source=.;Initial Catalog=Projet_GS;Integrated Security=True");
+
+        SqlDataAdapter da;
+        DataSet ds;
+        BindingSource Tbiding = new BindingSource();
+
+        int pos = -1;
+        int i;
+
+        public FrmDC()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDC_Load(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("select * from tblGS", ctn);
+            da = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            da.Fill(ds, "Auteurs");
+
+            actualiser();
+
+            //Tbiding.DataSource = ds.Tables["Auteurs"];
+            //txtID.DataBindings.Add(new Binding("Text", Tbiding, "ID"));
+            //txtNom.DataBindings.Add(new Binding("Text", Tbiding, "Nom"));
+            //txtAuteur.DataBindings.Add(new Binding("Text", Tbiding, "Auteur"));
+            //txtnbrpage.DataBindings.Add(new Binding("Text", Tbiding, "nbrPage"));
+            //txtprix.DataBindings.Add(new Binding("Text", Tbiding, "Prix"));
+        }
+        private void actualiser()  //Hakka Fonction wla Code Ghadi yb9a Yt3aoud Kandirha f blassa waheda  
+        {
+            this.dgv.Rows.Clear();
+            dgv.DataSource = ds.Tables["Auteurs"];
+        }
+        private void chemp()
+        {
+            txtID.Text = "";
+            txtNom.Text = "";
+            txtAuteur.Text = "";
+            txtnbrpage.Text = "";
+            txtprix.Text = "";
+        }
+       
+        private void test()
+        {
+
+            SqlCommand cmd = new SqlCommand("select * from tblGS where ID='" + txtID.Text + "'", ctn);
+            da = new SqlDataAdapter(cmd);
+            ds = new DataSet();
+            da.Fill(ds, "Auteurs");
+         
+
+
+        }
+
+        private void btnAjoute_Click(object sender, EventArgs e)
+        {
+            ds.Tables["Auteurs"].Rows.Add(txtID.Text, txtNom.Text, txtAuteur.Text, txtnbrpage.Text,txtprix.Text);
+            MessageBox.Show("Ajouter bien effected");
+            chemp();
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            //enregistrer sur bd :
+            SqlCommandBuilder bld = new SqlCommandBuilder(da);
+            bld.GetUpdateCommand();
+            da.Update(ds.Tables["Auteurs"]);
+            chemp();
+        }
+
+        private void btnModifie_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < ds.Tables["Auteurs"].Rows.Count; i++)
+            {
+                if (txtID.Text == ds.Tables["Auteurs"].Rows[i][0].ToString())
+                {
+                    pos = i;
+                    break;
+                }
+
+            }
+            ds.Tables["Auteurs"].Rows[pos][1] = txtNom.Text;
+            ds.Tables["Auteurs"].Rows[pos][2] = txtAuteur.Text;
+            ds.Tables["Auteurs"].Rows[pos][3] = txtnbrpage.Text;
+            ds.Tables["Auteurs"].Rows[pos][4] = txtprix.Text;
+            MessageBox.Show("modefication effected");
+            chemp();
+        }
+
+        private void btnsupprimer_Click(object sender, EventArgs e)
+        {
+            for (i = 0; i < ds.Tables["Auteurs"].Rows.Count; i++)
+            {
+                if (txtID.Text == ds.Tables["Auteurs"].Rows[i][0].ToString())
+                {
+                    pos = i;
+                    break;
+                }
+
+            }
+            ds.Tables["Auteurs"].Rows[pos].Delete();
+            MessageBox.Show("Bien supprimer");
+            chemp();
+        }
+
+        private void btnRecherche_Click(object sender, EventArgs e)
+        {
+            //hadi Jedida Ya3ni fach tcllike 3la btn otkounkon khaouia ki ytla3 lik icon ki ygol lik 3ameriha 
+            errorProvider1.Clear();
+            if(txtID.Text == "")
+            {
+                errorProvider1.SetError(txtID, "Entre ID");
+                return;
+            }
+            //Hadi Methoude Hya fash Kankon dayerin code na9ess wla 3arf ytla3 Probleme donc Bash Yb9a Programe Khedam blama you9e3 Prob kakhdemdo b fonction Try catch
+            try 
+            {
+          
+                for (i = 0; i < ds.Tables["Auteurs"].Rows.Count; i++)
+                {
+                    if (txtID.Text == ds.Tables["Auteurs"].Rows[i][0].ToString())
+                    {
+                        pos = i;
+                        break;
+                    }
+                }
+                txtID.Text = ds.Tables["Auteurs"].Rows[pos][0].ToString();
+                txtNom.Text = ds.Tables["Auteurs"].Rows[pos][1].ToString();
+                txtAuteur.Text = ds.Tables["Auteurs"].Rows[pos][2].ToString();
+                txtnbrpage.Text = ds.Tables["Auteurs"].Rows[pos][3].ToString();
+                txtprix.Text = ds.Tables["Auteurs"].Rows[pos][4].ToString();
+            }catch
+            {
+                MessageBox.Show("ID n'exciste Pas! ");
+            }
+
+
+
+        }
+
+        private void dgv_Click(object sender, EventArgs e)
+        {
+            //Hada Code Dyal fash Ila clik 3la wahed Men li 3andi f dgv Ytla3 lia F mektoubin f textBox Bash Momkin Modifié
+            if (dgv.SelectedRows.Count > 0)
+            {
+                txtID.Text = dgv.SelectedRows[0].Cells[0].Value.ToString();
+                txtNom.Text = dgv.SelectedRows[0].Cells[1].Value.ToString();
+                txtAuteur.Text = dgv.SelectedRows[0].Cells[2].Value.ToString();
+                txtnbrpage.Text = dgv.SelectedRows[0].Cells[3].Value.ToString();
+                txtprix.Text = dgv.SelectedRows[0].Cells[4].Value.ToString();
+            }
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+    }
+}
---
+++ FrmDC.designer.cs
@@ -0,0 +1,293 @@
+﻿namespace AdoNet_V3
+{
+    partial class FrmDC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAuteur = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtprix = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnbrpage = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnAjoute = new System.Windows.Forms.Button();
+            this.btnModifie = new System.Windows.Forms.Button();
+            this.btnsupprimer = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btnRecherche = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(60, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 24);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Prix";
+            // 
+            // txtAuteur
+            // 
+            this.txtAuteur.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAuteur.Location = new System.Drawing.Point(182, 194);
+            this.txtAuteur.Name = "txtAuteur";
+            this.txtAuteur.Size = new System.Drawing.Size(138, 23);
+            this.txtAuteur.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(60, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID";
+            // 
+            // txtprix
+            // 
+            this.txtprix.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtprix.Location = new System.Drawing.Point(182, 310);
+            this.txtprix.Name = "txtprix";
+            this.txtprix.Size = new System.Drawing.Size(138, 23);
+            this.txtprix.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(60, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nom";
+            // 
+            // txtnbrpage
+            // 
+            this.txtnbrpage.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtnbrpage.Location = new System.Drawing.Point(182, 249);
+            this.txtnbrpage.Name = "txtnbrpage";
+            this.txtnbrpage.Size = new System.Drawing.Size(138, 23);
+            this.txtnbrpage.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(130, 24);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "N° De Page";
+            // 
+            // txtNom
+            // 
+            this.txtNom.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNom.Location = new System.Drawing.Point(182, 148);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(138, 23);
+            this.txtNom.TabIndex = 10;
+            this.txtNom.Tag = "ED";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(78, 24);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Auteur";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(182, 93);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(100, 23);
+            this.txtID.TabIndex = 11;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // btnAjoute
+            // 
+            this.btnAjoute.Location = new System.Drawing.Point(40, 366);
+            this.btnAjoute.Name = "btnAjoute";
+            this.btnAjoute.Size = new System.Drawing.Size(141, 35);
+            this.btnAjoute.TabIndex = 12;
+            this.btnAjoute.Text = "Ajoute";
+            this.btnAjoute.UseVisualStyleBackColor = true;
+            this.btnAjoute.Click += new System.EventHandler(this.btnAjoute_Click);
+            // 
+            // btnModifie
+            // 
+            this.btnModifie.Location = new System.Drawing.Point(187, 366);
+            this.btnModifie.Name = "btnModifie";
+            this.btnModifie.Size = new System.Drawing.Size(141, 35);
+            this.btnModifie.TabIndex = 12;
+            this.btnModifie.Text = "Modifier";
+            this.btnModifie.UseVisualStyleBackColor = true;
+            this.btnModifie.Click += new System.EventHandler(this.btnModifie_Click);
+            // 
+            // btnsupprimer
+            // 
+            this.btnsupprimer.Location = new System.Drawing.Point(187, 417);
+            this.btnsupprimer.Name = "btnsupprimer";
+            this.btnsupprimer.Size = new System.Drawing.Size(141, 35);
+            this.btnsupprimer.TabIndex = 12;
+            this.btnsupprimer.Text = "Supprimer";
+            this.btnsupprimer.UseVisualStyleBackColor = true;
+            this.btnsupprimer.Click += new System.EventHandler(this.btnsupprimer_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(369, 148);
+            this.dgv.Name = "dgv";
+            this.dgv.ReadOnly = true;
+            this.dgv.Size = new System.Drawing.Size(537, 264);
+            this.dgv.TabIndex = 13;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            this.dgv.Click += new System.EventHandler(this.dgv_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(40, 417);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(141, 35);
+            this.btnupdate.TabIndex = 12;
+            this.btnupdate.Text = "Enregistrer sur bd";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btnRecherche
+            // 
+            this.btnRecherche.Location = new System.Drawing.Point(369, 85);
+            this.btnRecherche.Name = "btnRecherche";
+            this.btnRecherche.Size = new System.Drawing.Size(141, 35);
+            this.btnRecherche.TabIndex = 12;
+            this.btnRecherche.Text = "Recherche par ID";
+            this.btnRecherche.UseVisualStyleBackColor = true;
+            this.btnRecherche.Click += new System.EventHandler(this.btnRecherche_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(175, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 38);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Gestion Des Livres";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(493, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(302, 38);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Mode Déconecté";
+            // 
+            // FrmDC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(928, 476);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnsupprimer);
+            this.Controls.Add(this.btnRecherche);
+            this.Controls.Add(this.btnModifie);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnAjoute);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAuteur);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtprix);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtnbrpage);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtID);
+            this.Name = "FrmDC";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmDC";
+            this.Load += new System.EventHandler(this.FrmDC_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAuteur;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtprix;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnbrpage;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnsupprimer;
+        private System.Windows.Forms.Button btnModifie;
+        private System.Windows.Forms.Button btnAjoute;
+        private System.Windows.Forms.Button btnRecherche;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label1;
+    }
+}
